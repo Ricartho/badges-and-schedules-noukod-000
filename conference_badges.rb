0ec06b1f9067e1d447bea9261624e8c0aab5d2ab
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-12 13:31:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def badge_maker(name)
+  
+   return "Hello, my name is #{name}."
+
+end 
+
+def batch_badge_creator(name_array)
+new_array = []
+  name_array.each do |speaker|
+    new_array << badge_maker(speaker)
+    end
+    return new_array
+end 
